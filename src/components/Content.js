561601c4db33c25eaf30440af9a10bd4d394884c
Timothy Moore <moore.tim@outlook.com>
2018-03-21 21:20:09 -0400

    Use dompurify to sanitize post html

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+const createDOMPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
 
-export default ({ content, className }) => (
-  <div className={className}>{content}</div>
-)
-export const HTMLContent = ({ content, className }) => (
-  <div className={className} dangerouslySetInnerHTML={{ __html: content }} />
-)
+const window = new JSDOM('').window;
+const DOMPurify = createDOMPurify(window);
+
+export default ({ content, className }) => {
+  return <div className={className}>{content}</div>;
+};
+export const HTMLContent = ({ content, className }) => {
+  const cleanContent = DOMPurify.sanitize(content);
+  return (
+    <div
+      className={className}
+      dangerouslySetInnerHTML={{ __html: cleanContent }}
+    />
+  );
+};
