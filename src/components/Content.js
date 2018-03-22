129653ab90416a60eeb467e30b5f5583bdd7315d
Timothy Moore <moore.tim@outlook.com>
2018-03-21 21:28:53 -0400

    Remove jsdom

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,8 +1,6 @@
 import React from 'react';
 const createDOMPurify = require('dompurify');
-const { JSDOM } = require('jsdom');
 
-const window = new JSDOM('').window;
 const DOMPurify = createDOMPurify(window);
 
 export default ({ content, className }) => {
