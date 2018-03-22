a2a90925f8e84bb45075ab751d5dea7bdea8f8d0
Timothy Moore <moore.tim@outlook.com>
2018-03-21 22:11:10 -0400

    Update dompurify setup

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,7 +1,5 @@
 import React from 'react';
-const createDOMPurify = require('dompurify');
-
-const DOMPurify = createDOMPurify();
+require('../../purify.min.js');
 
 export default ({ content, className }) => {
   return <div className={className}>{content}</div>;
